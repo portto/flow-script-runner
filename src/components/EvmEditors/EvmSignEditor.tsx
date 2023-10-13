51c58526b2b9582efe1980222dcb173aa16c418c
calvinchang <q20274982@gmail.com>
2023-10-13 15:09:08 +0800

    feat: remove eth_sign feature

--- src/components/EvmEditors/EvmSignEditor.tsx
+++ src/components/EvmEditors/EvmSignEditor.tsx
@@ -22,38 +22,6 @@ function parseJsonString(str: string) {
   }
 }
 
-const EthSignEditor = ({
-  account,
-  setRequestObject,
-}: {
-  account: string | null;
-  setRequestObject: Dispatch<
-    SetStateAction<EthereumTypes.EIP1193RequestPayload | undefined>
-  >;
-}): ReactJSXElement => {
-  const [message, setMessage] = useState<string>("test");
-
-  useEffect(() => {
-    setRequestObject({
-      method: "eth_sign",
-      params: [account, "0x" + Buffer.from(message).toString("hex")],
-    });
-  }, [message, account, setRequestObject]);
-
-  return (
-    <Flex my="20px" alignItems="center">
-      <Box mx="10px">Message:</Box>
-      <Textarea
-        rows={1}
-        value={message}
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
-      />
-    </Flex>
-  );
-};
-
 const PersonalSignEditor = ({
   account,
   setRequestObject,
@@ -302,13 +270,6 @@ const EvmSignEditor = ({
           >
             signTypedData_v4
           </Tab>
-          <Tab
-            onClick={() => {
-              setMethod("eth_sign");
-            }}
-          >
-            eth_sign
-          </Tab>
         </TabList>
 
         <TabPanels>
@@ -323,12 +284,6 @@ const EvmSignEditor = ({
           </TabPanel>
           <TabPanel p="0">
             <SignTypedDataV4Editor setParam={setParam} />
-          </TabPanel>
-          <TabPanel p="0">
-            <EthSignEditor
-              account={account}
-              setRequestObject={setRequestObject}
-            />
           </TabPanel>
         </TabPanels>
       </Tabs>
